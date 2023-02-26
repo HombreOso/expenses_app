febcb5e0fe804bfff2b11b28db6ad2f36cee10de
ArthurEmig <arthur.erich.emig@gmail.com>
2023-02-26 22:49:16 +0100

    registered on firebase

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -38,7 +38,7 @@ class _NewTransactionState extends State<NewTransaction> {
     showDatePicker(
       context: context,
       initialDate: DateTime.now(),
-      firstDate: DateTime(2019),
+      firstDate: DateTime(2023),
       lastDate: DateTime.now(),
     ).then((pickedDate) {
       if (pickedDate == null) {
