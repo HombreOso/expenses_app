17c2b8a16b7aa1fe90625f6ac4636a2b19c56e32
ArthurEmig <arthur.erich.emig@gmail.com>
2023-04-08 16:07:47 +0200

    introducing Dismissible widget to transaction list tiles

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -33,36 +33,65 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
+                return Dismissible(
+                  key: Key(transactions[index].id),
+                  background: Container(
+                    color: Theme.of(context).colorScheme.error,
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                      size: 40,
+                    ),
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(right: 20),
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 4,
+                    ),
                   ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text('\$${transactions[index].amount}'),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (direction) {
+                    deleteTx(
+                      transactions[index].id,
+                      transactions[index].uid,
+                    );
+                  },
+                  child: Card(
+                    elevation: 5,
+                    margin: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 5,
+                    ),
+                    child: GestureDetector(
+                      onTap: () {
+                        // Handle edit transaction here
+                        print('Edit transaction ${transactions[index].id}');
+                      },
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          child: Padding(
+                            padding: EdgeInsets.all(6),
+                            child: FittedBox(
+                              child: Text('\$${transactions[index].amount}'),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).colorScheme.error,
-                      onPressed: () => deleteTx(
-                        transactions,
-                        transactions[index].id,
-                        transactions[index].uid,
+                        title: Text(
+                          transactions[index].title,
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        subtitle: Text(
+                          DateFormat.yMMMd().format(transactions[index].date),
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.edit),
+                          color: Colors.blueAccent,
+                          onPressed: () {
+                            // Handle edit transaction here
+                            print('Edit transaction ${transactions[index].id}');
+                          },
+                        ),
                       ),
                     ),
                   ),
